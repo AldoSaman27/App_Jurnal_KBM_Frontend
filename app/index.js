a98f8fd95e9b81c4fced666537b199f3dee48dae
Aldo Saman <aldosaman711@gmail.com>
2024-03-05 00:17:41 +0700

    refactor: Added Welcome page

--- app/index.js
+++ app/index.js
@@ -1,51 +1,93 @@
-import { View, Text } from "react-native";
-import { useEffect, useState } from "react";
+import { Link } from "expo-router";
+import { useEffect } from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-
-// Components
-import fetchFromAsyncStorage from "../components/fetchFromAsyncStorage";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const index = () => {
-  const [storageData, setStorageData] = useState({
-    accessToken: null,
-    id: null,
-    name: null,
-    nip: null,
-    mapel: null,
-    foto: null,
-    email: null,
-    created_at: null,
-    updated_at: null,
-  });
-
-  const {
-    accessToken,
-    id,
-    name,
-    nip,
-    mapel,
-    foto,
-    email,
-    created_at,
-    updated_at,
-  } = storageData;
-
   useEffect(() => {
-    const fetchData = async () => {
-      const fetchedData = await fetchFromAsyncStorage();
-      setStorageData(fetchedData);
-    };
-
-    fetchData();
-  }, []);
+    AsyncStorage.clear();
+  }, [AsyncStorage]);
 
   return (
     <SafeAreaProvider>
-      <View>
-        <Text>{accessToken}</Text>
+      <View style={styles.container}>
+        <Ionicons name="journal" size={100} color="#2099FF" />
+        <Text style={styles.heading_text}>Selamat datang di Jurnal!</Text>
+        <Text style={styles.description_text}>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores
+          error molestiae eum architecto deleniti minus.
+        </Text>
+        <TouchableOpacity style={styles.button_group}>
+          <Link href={"/auth/login"} style={styles.button_group_text}>
+            Log In
+          </Link>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button_group_2}>
+          <Link href={"/auth/register"} style={styles.button_group_2_text}>
+            Register
+          </Link>
+        </TouchableOpacity>
       </View>
     </SafeAreaProvider>
   );
 };
 
 export default index;
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    height: "100%",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingHorizontal: 20,
+  },
+  heading_text: {
+    marginTop: 20,
+    fontSize: 20,
+    fontWeight: "500",
+  },
+  description_text: {
+    textAlign: "center",
+    marginTop: 10,
+    marginBottom: 20,
+  },
+  button_group: {
+    backgroundColor: "#2099FF",
+    width: "100%",
+    height: 50,
+    borderRadius: 10,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    marginVertical: 5,
+  },
+  button_group_text: {
+    fontSize: 15,
+    fontWeight: "900",
+    color: "#FFF",
+    width: "100%",
+    textAlign: "center",
+  },
+  button_group_2: {
+    borderWidth: 5,
+    borderColor: "#2099FF",
+    width: "100%",
+    height: 50,
+    borderRadius: 10,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    marginVertical: 5,
+  },
+  button_group_2_text: {
+    fontSize: 15,
+    fontWeight: "900",
+    color: "#2099FF",
+    width: "100%",
+    textAlign: "center",
+  },
+});

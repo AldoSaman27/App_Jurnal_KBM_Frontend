1ed496da6e4325ea6a8393d1bbab65e2ca8bec1c
Aldo Saman <86990025+AldoSaman27@users.noreply.github.com>
2024-12-12 09:12:40 +0700

    refactor: Restructure file getJurnalData.js

--- components/getJurnalData.js
+++ components/getJurnalData.js
@@ -1,18 +1,15 @@
+// axios
 import axios from "axios";
 
 const getJurnalData = async (accessToken, nip, date) => {
-  try {
-    axios.defaults.headers.common["Authorization"] = `Bearer ${accessToken}`;
-    axios.defaults.headers.common["Accept"] = "application/json";
-    const response = await axios.get(
-      `${process.env.EXPO_PUBLIC_API_URL}/api/jurnal/index/${nip}/${
-        date.getMonth() + 1
-      }/${date.getFullYear()}`
-    );
-    return response.data.jurnal;
-  } catch (error) {
-    throw error.response.data; // Melemparkan kesalahan yang ditangkap agar dapat ditangani di tempat lain
-  }
+    try {
+        axios.defaults.headers.common["Authorization"] = `Bearer ${accessToken}`;
+        axios.defaults.headers.common["Accept"] = "application/json";
+        const response = await axios.get(`${process.env.EXPO_PUBLIC_API_URL}/api/jurnal/index/${nip}/${date.getMonth() + 1}/${date.getFullYear()}`);
+        return response.data.jurnal;
+    } catch (error) {
+        throw error.response.data;
+    }
 };
 
 export default getJurnalData;

19455f4506de9ef7651a79d0e427684635ca92f3
Aldo Saman <86990025+AldoSaman27@users.noreply.github.com>
2024-12-12 09:30:53 +0700

    refactor: Restructure file _layout.js

--- app/_layout.js
+++ app/_layout.js
@@ -1,44 +1,45 @@
+// axios
 import axios from "axios";
+
+// react
 import { useEffect } from "react";
+
+// expo-router
 import { Stack, SplashScreen, router } from "expo-router";
 
-// Components
+// fetchFromAsyncStorage
 import fetchFromAsyncStorage from "../components/fetchFromAsyncStorage";
 
 SplashScreen.preventAutoHideAsync();
 
 export default function layout() {
-  useEffect(() => {
-    const fetchData = async () => {
-      const fetchedData = await fetchFromAsyncStorage();
-      const accessToken = fetchedData.accessToken;
+    useEffect(() => {
+        const fetchData = async () => {
+            const fetchedData = await fetchFromAsyncStorage();
+            const accessToken = fetchedData.accessToken;
 
-      axios.defaults.headers.common["Authorization"] = `Bearer ${accessToken}`;
-      axios.defaults.headers.common["Accept"] = `application/json`;
-      await axios
-        .get(`${process.env.EXPO_PUBLIC_API_URL}/api/auth/checktoken`)
-        .then(() => {
-          router.replace("/(tabs)/dashboard");
-        })
-        .catch(() => {
-          router.replace("/");
-        })
-        .finally(() => {
-          setTimeout(() => {
-            SplashScreen.hideAsync();
-          }, 2500);
-        });
-    };
+            axios.defaults.headers.common["Authorization"] = `Bearer ${accessToken}`;
+            axios.defaults.headers.common["Accept"] = `application/json`;
+            await axios.get(`${process.env.EXPO_PUBLIC_API_URL}/api/user`).then(() => {
+                router.replace("/(tabs)/dashboard");
+            }).catch(() => {
+                router.replace("/");
+            }).finally(() => {
+                setTimeout(() => {
+                    SplashScreen.hideAsync();
+                }, 2500);
+            });
+        };
 
-    fetchData();
-  }, []);
+        fetchData();
+    }, []);
 
-  return (
-    <Stack screenOptions={{ headerTitle: "" }}>
-      <Stack.Screen name="auth/login" options={{ headerShown: false }} />
-      <Stack.Screen name="auth/register" options={{ headerShown: false }} />
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
-  );
+    return (
+        <Stack screenOptions={{ headerTitle: "" }}>
+            <Stack.Screen name="auth/login" options={{ headerShown: false }} />
+            <Stack.Screen name="auth/register" options={{ headerShown: false }} />
+            <Stack.Screen name="index" options={{ headerShown: false }} />
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        </Stack>
+    );
 }

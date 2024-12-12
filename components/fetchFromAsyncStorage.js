9f997f7b1cd59d59ccb9dd483f7f7f4b3100fcab
Aldo Saman <86990025+AldoSaman27@users.noreply.github.com>
2024-12-12 09:07:49 +0700

    refactor: Restructure file fetchFromAsyncStorage.js

--- components/fetchFromAsyncStorage.js
+++ components/fetchFromAsyncStorage.js
@@ -1,29 +1,30 @@
+// async-storage
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const fetchFromAsyncStorage = async () => {
-  const fetchAccessToken = await AsyncStorage.getItem("accessToken");
-  const fetchId = await AsyncStorage.getItem("id");
-  const fetchName = await AsyncStorage.getItem("name");
-  const fetchNip = await AsyncStorage.getItem("nip");
-  const fetchMapel = await AsyncStorage.getItem("mapel");
-  const fetchSekolah = await AsyncStorage.getItem("sekolah");
-  const fetchFoto = await AsyncStorage.getItem("foto");
-  const fetchEmail = await AsyncStorage.getItem("email");
-  const fetchCreated_at = await AsyncStorage.getItem("created_at");
-  const fetchUpdated_at = await AsyncStorage.getItem("updated_at");
+    const fetchAccessToken = await AsyncStorage.getItem("accessToken");
+    const fetchId = await AsyncStorage.getItem("id");
+    const fetchName = await AsyncStorage.getItem("name");
+    const fetchNip = await AsyncStorage.getItem("nip");
+    const fetchMapel = await AsyncStorage.getItem("mapel");
+    const fetchSekolah = await AsyncStorage.getItem("sekolah");
+    const fetchFoto = await AsyncStorage.getItem("foto");
+    const fetchEmail = await AsyncStorage.getItem("email");
+    const fetchCreated_at = await AsyncStorage.getItem("created_at");
+    const fetchUpdated_at = await AsyncStorage.getItem("updated_at");
 
-  return {
-    accessToken: fetchAccessToken,
-    id: fetchId,
-    name: fetchName,
-    nip: fetchNip,
-    mapel: fetchMapel,
-    sekolah: fetchSekolah,
-    foto: fetchFoto,
-    email: fetchEmail,
-    created_at: fetchCreated_at,
-    updated_at: fetchUpdated_at,
-  };
+    return {
+        accessToken: fetchAccessToken,
+        id: fetchId,
+        name: fetchName,
+        nip: fetchNip,
+        mapel: fetchMapel,
+        sekolah: fetchSekolah,
+        foto: fetchFoto,
+        email: fetchEmail,
+        created_at: fetchCreated_at,
+        updated_at: fetchUpdated_at,
+    };
 };
 
 export default fetchFromAsyncStorage;

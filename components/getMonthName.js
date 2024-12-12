6498576e44eb8c916fdd1f7423e978d8105fbc72
Aldo Saman <86990025+AldoSaman27@users.noreply.github.com>
2024-12-12 09:13:08 +0700

    refactor: Restructure file getMonthName.js

--- components/getMonthName.js
+++ components/getMonthName.js
@@ -1,19 +1,20 @@
 const getMonthName = (monthNumber) => {
-  const months = [
-    "Januari",
-    "Februari",
-    "Maret",
-    "April",
-    "Mei",
-    "Juni",
-    "Juli",
-    "Agustus",
-    "September",
-    "Oktober",
-    "November",
-    "Desember",
-  ];
-  return months[monthNumber];
+    const months = [
+        "Januari",
+        "Februari",
+        "Maret",
+        "April",
+        "Mei",
+        "Juni",
+        "Juli",
+        "Agustus",
+        "September",
+        "Oktober",
+        "November",
+        "Desember",
+    ];
+    
+    return months[monthNumber];
 };
 
 export default getMonthName;

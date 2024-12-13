aa437e2a96024447dc0a3541ac554b2f33fe5057
Aldo Saman <86990025+AldoSaman27@users.noreply.github.com>
2024-12-13 08:16:26 +0700

    refactor: Restructure file _layout.js

--- app/(tabs)/_layout.js
+++ app/(tabs)/_layout.js
@@ -1,31 +1,28 @@
+// expo-router
 import { Tabs } from "expo-router";
 
-// Icon
+// FontAwesome
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 
 export default function TabLayout() {
-  return (
-    <Tabs screenOptions={{ tabBarActiveTintColor: "#2099FF" }}>
-      <Tabs.Screen
-        name="dashboard/index"
-        options={{
-          title: "Dashboard",
-          headerShown: false,
-          tabBarIcon: ({ color }) => (
-            <FontAwesome size={30} name="home" color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="account/index"
-        options={{
-          title: "Account",
-          headerShown: false,
-          tabBarIcon: ({ color }) => (
-            <FontAwesome size={30} name="user" color={color} />
-          ),
-        }}
-      />
-    </Tabs>
-  );
+    return (
+        <Tabs screenOptions={{ tabBarActiveTintColor: "#2099FF" }}>
+            <Tabs.Screen name="dashboard/index" options={{
+                    title: "Dashboard",
+                    headerShown: false,
+                    tabBarIcon: ({ color }) => (
+                        <FontAwesome size={30} name="home" color={color} />
+                    ),
+                }}
+            />
+            <Tabs.Screen name="account/index" options={{
+                    title: "Account",
+                    headerShown: false,
+                    tabBarIcon: ({ color }) => (
+                        <FontAwesome size={30} name="user" color={color} />
+                    ),
+                }}
+            />
+        </Tabs>
+    );
 }
